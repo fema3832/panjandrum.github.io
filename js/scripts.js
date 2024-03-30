0feb1a0ff260548ff5f4f588901bf990588a3339
Máté Fejes <fema3832@gmail.com>
2024-03-30 12:35:52 +0100

    upgraded seo, merge css

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,23 @@
 // Scripts
 // 
 
+// Create the observer like the examples above
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate-animation');
+        entry.target.classList.remove('animate');
+        return;
+      }
+    });
+  });
+  
+  // Get multiple elements instead of a single one using "querySelectorAll"
+  const squares = document.querySelectorAll('.animate');
+  
+  // Loop over the elements and add each one to the observer
+  squares.forEach((element) => observer.observe(element));
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
